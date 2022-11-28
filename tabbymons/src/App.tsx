2c95f6b5cce1577c00644b4560817fbb00af84e3
Matthew Zhou <myzhou@uwaterloo.ca>
2022-11-28 12:42:59 -0500

    Add shiny pokemon

--- tabbymons/src/App.tsx
+++ tabbymons/src/App.tsx
@@ -11,18 +11,27 @@ function App() {
   const Pokedex = require("pokeapi-js-wrapper")
   const P = new Pokedex.Pokedex()
   
-  P.getPokemonByName("eevee")
-  .then((response: any) => {
+  P.getPokemonSpeciesByName(25).then(function(response: any) {
     console.log(response)
   })
 
-  const pokedexNumber = getRandNum(1, 905);
-  const image = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + pokedexNumber + ".png";
+  //TODO: Different rates for rarer pokemon. 
+  //IDEA: Can set spawns to different reigons. We could even do different routes like in a pokemon game.
+  //extension: Spawns can even be correlated to time of day. i.e. Solrock in day, lunatone at night.
+  
+  const pokedexNumber = getRandNum(1, 905); // Currently 905 Pokemon in Pokedex; update when new generations come out.
+  const shinyChance = getRandNum(1,20); // Shiny rate. It's currently set lower for fun.
+
+  let image =  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+  
+  if (shinyChance == 20) {
+     image += "shiny/" + pokedexNumber + ".png";
+  } else {
+    image += pokedexNumber + ".png";
+  }
   console.log(image);
 
   return (
-    
-    
     <div className="App">
       <header className="App-header">
         <img src={image} className="App-logo" alt="logo" />
